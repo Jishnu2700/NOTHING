7626e894a4708620fe63b928ca2ed8d286c4696a
Jishnu2700 <77037764+Jishnu2700@users.noreply.github.com>
2021-02-12 13:34:31 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60263600372cb7001f0e33fb",
+				"60263600372cb7001f0e33fd"
+			]
+		},
+		"60263600372cb7001f0e33fb": {
+			"id": "60263600372cb7001f0e33fb",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60263600372cb7001f0e33fd": {
+			"id": "60263600372cb7001f0e33fd",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
